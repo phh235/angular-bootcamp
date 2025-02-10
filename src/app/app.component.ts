4f299ac2c065284e13252411bf8b998fb49bf6dc
Andrew Hoang <hoangaatrox23504@gmail.com>
2025-02-10 22:58:15 +0700

    Add NgRx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderLayoutComponent } from './shared/components/HeaderLayout/header-layout.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from './stores/app.state';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule, CommonModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  counter$: Observable<number>;
 
+  constructor(private store: Store<AppState>) {
+    this.counter$ = this.store.select(state => state.counter);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { reducers } from './stores';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),
-  provideHttpClient(withFetch()),]
+  provideHttpClient(withFetch()), provideStore(reducers)]
 };
